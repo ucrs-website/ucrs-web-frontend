99195b1a691790b9d7c2123fcd5944a5bed4877d
Alireza-Moradian <a.moradian@ucrs.com>
2025-11-02 13:45:24 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { reactCompiler: false },
-
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
@@ -9,12 +8,10 @@ const nextConfig = {
     minimumCacheTTL: 60,
     remotePatterns: [],
   },
-
   output: 'standalone',
   compress: true,
   poweredByHeader: false,
   trailingSlash: false,
   productionBrowserSourceMaps: false,
 };
-
 export default nextConfig;
